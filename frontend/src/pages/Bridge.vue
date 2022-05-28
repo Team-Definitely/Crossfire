437e78765458bba12f52bc45f55dafe697f8342e
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-05-29 03:35:51 +0530

    fix: output amount

--- frontend/src/pages/Bridge.vue
+++ frontend/src/pages/Bridge.vue
@@ -107,7 +107,7 @@ function transfer() {
             <InputField label="From token" v-model="selectedToken0" v-model:inputValue="inputAmount" placeholder="0.0"
                 :list="tokenList0" v-debounce:300ms="getQuote" />
             <div class="my-1.5 w-full text-center">&darr;</div>
-            <InputField label="To token" v-model="selectedToken1" v-model:inputValue="inputAmount" placeholder="0.0"
+            <InputField label="To token" v-model="selectedToken1" v-model:inputValue="outputAmount" placeholder="0.0"
                 :list="tokenList1" disabled="true" />
         </div>
 
