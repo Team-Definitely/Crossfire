f11126d8c887738ba6550def32c33a6954834dbe
Jayesh Nirve <nirvejayesh@gmail.com>
2022-05-29 01:14:22 +0530

    fix: token images

--- frontend/src/utils/icons.ts
+++ frontend/src/utils/icons.ts
@@ -1,31 +1,34 @@
-import { logos, logo_aliases } from '../constants'
-export function getIconUrl(name: string, type: "Chain" | "Token" = "Chain"): string {
-    if (!name) return ''
-    if (name && Object.keys(logos).includes(name.toLowerCase())) {
-        return logos[name.toLowerCase()]
-    }
+/* eslint-disable camelcase */
+// eslint-disable-next-line node/no-missing-import
+import { logos, logo_aliases } from "../constants";
+export function getIconUrl(
+  name: string,
+  type: "Chain" | "Token" = "Chain"
+): string {
+  if (!name) return "";
+  if (name && Object.keys(logos).includes(name.toLowerCase())) {
+    return logos[name.toLowerCase()];
+  }
 
-    if (name && Object.keys(logo_aliases).includes(name.toLowerCase())) {
-        name = logo_aliases[name.toLowerCase()]
-    }
+  if (name && Object.keys(logo_aliases).includes(name.toLowerCase())) {
+    name = logo_aliases[name.toLowerCase()];
+  }
 
-    // if (name === 'WETH') name = 'ETH'
-    // const BASE_URL =
-    //     'https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/svg/icon/'
-    const BASE_URL1 =
-        'https://movricons.s3.ap-south-1.amazonaws.com/'
-    const BASE_URL2 =
-        `https://maticnetwork.github.io/polygon-token-assets/assets/`
+  // if (name === 'WETH') name = 'ETH'
+  // const BASE_URL =
+  //     'https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/svg/icon/'
+  const BASE_URL1 = "https://movricons.s3.ap-south-1.amazonaws.com/";
+  const BASE_URL2 = `https://maticnetwork.github.io/polygon-token-assets/assets/`;
 
+  const BASE_URL = type === "Chain" ? BASE_URL1 : BASE_URL2;
+  if (BASE_URL === BASE_URL2) {
+    name = name.toLocaleLowerCase();
+  }
+  const result = `${BASE_URL}${name}.svg`;
 
-    let BASE_URL = type === 'Chain' ? BASE_URL1 : BASE_URL2
-    console.log("BASE URL", BASE_URL, type)
-
-    const result = `${BASE_URL}${name}.svg`
-
-    return result
+  return result;
 }
 
 export function onImageError(event: any) {
-    event.target.src = '/ban.svg'
+  event.target.src = "/ban.svg";
 }
