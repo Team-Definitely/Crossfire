b679261d55a4ca675557d4a00836919d4a40a89a
Jayesh Nirve <nirvejayesh@gmail.com>
2022-05-29 02:02:22 +0530

    feat: search field

--- frontend/src/components/Dropdown.vue
+++ frontend/src/components/Dropdown.vue
@@ -73,9 +73,57 @@ function select(item: any) {
         </span>
     </button>
     <Modal v-model="open" :close="closeModal">
-        <div class="bg-true-gray-700 text-white rounded-lg w-10/12 md:w-1/5">
+        <div class="bg-true-gray-700 text-white rounded-lg w-96">
             <div class="p-5 bg-true-gray-800 rounded-t-lg">
                 Select a {{ props.type }}
+            </div>
+            <div class="px-5 pt-4 pb-2 border-b border-gray-200">
+                <div
+                    class="flex items-center px-2 w-full bg-gray-100 border-gray-100 rounded-lg text-gray-800 sm:text-sm overflow-hidden focus-within:border-secondary mb-4 focus-within:bg-white relative">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="w-5 h-5 text-gray-500 mr-2">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                    </svg>
+                    <input id="search_address" placeholder="Search Name or Address"
+                        class="w-full border-none py-2.5 bg-transparent" role="search" spellcheck="false" value="">
+                </div>
+                <div class="flex flex-wrap gap-x-2 gap-y-2 mb-4">
+                    <div
+                        class="text-black flex items-center border border-gray-200 bg-gray-50 cursor-pointer hover:bg-gray-100 rounded-full py-1 pl-1 pr-2 sm:py-2 sm:pl-2 sm:pr-4 text-xs font-medium">
+                        <img class="w-6 h-6 rounded-full mr-1 sm:mr-2 bg-gray-200 border border-gray-300"
+                            src="https://maticnetwork.github.io/polygon-token-assets/assets/dai.svg">DAI
+                    </div>
+                    <div
+                        class="text-black flex items-center border border-gray-200 bg-gray-50 cursor-pointer hover:bg-gray-100 rounded-full py-1 pl-1 pr-2 sm:py-2 sm:pl-2 sm:pr-4 text-xs font-medium">
+                        <img class="w-6 h-6 rounded-full mr-1 sm:mr-2 bg-gray-200 border border-gray-300"
+                            src="https://maticnetwork.github.io/polygon-token-assets/assets/matic.svg">MATIC
+                    </div>
+                    <div
+                        class="text-black flex items-center border border-gray-200 bg-gray-50 cursor-pointer hover:bg-gray-100 rounded-full py-1 pl-1 pr-2 sm:py-2 sm:pl-2 sm:pr-4 text-xs font-medium">
+                        <img class="w-6 h-6 rounded-full mr-1 sm:mr-2 bg-gray-200 border border-gray-300"
+                            src="https://maticnetwork.github.io/polygon-token-assets/assets/usdc.svg">USDC
+                    </div>
+                    <div
+                        class="text-black flex items-center border border-gray-200 bg-gray-50 cursor-pointer hover:bg-gray-100 rounded-full py-1 pl-1 pr-2 sm:py-2 sm:pl-2 sm:pr-4 text-xs font-medium">
+                        <img class="w-6 h-6 rounded-full mr-1 sm:mr-2 bg-gray-200 border border-gray-300"
+                            src="https://maticnetwork.github.io/polygon-token-assets/assets/usdt.svg">USDT
+                    </div>
+                    <div
+                        class="text-black flex items-center border border-gray-200 bg-gray-50 cursor-pointer hover:bg-gray-100 rounded-full py-1 pl-1 pr-2 sm:py-2 sm:pl-2 sm:pr-4 text-xs font-medium">
+                        <img class="w-6 h-6 rounded-full mr-1 sm:mr-2 bg-gray-200 border border-gray-300"
+                            src="https://maticnetwork.github.io/polygon-token-assets/assets/wbtc.svg">WBTC
+                    </div>
+                    <div
+                        class="text-black flex items-center border border-gray-200 bg-gray-50 cursor-pointer hover:bg-gray-100 rounded-full py-1 pl-1 pr-2 sm:py-2 sm:pl-2 sm:pr-4 text-xs font-medium">
+                        <img class="w-6 h-6 rounded-full mr-1 sm:mr-2 bg-gray-200 border border-gray-300"
+                            src="https://maticnetwork.github.io/polygon-token-assets/assets/weth.svg">WETH
+                    </div>
+                </div>
+                <div class="flex justify-between items-center sticky top-0 text-xs font-medium text-gray-500 px-1">
+                    <span>Token name</span><span>Balance</span>
+                </div>
             </div>
             <ul class="list-none max-h-64 overflow-y-auto">
                 <li v-for="item in items" @click="select(item)"
